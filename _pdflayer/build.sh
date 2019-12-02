61e926aceae602a76e8b49bb90cc93afedaee7d9
EC2 Default User <ec2-user@ip-172-31-84-117.ec2.internal>
2019-12-02 06:29:47 +0000

    back to PC
    go back to amz linux 1, try chrono's python libs with jbig2enc, unpaper etc

--- _pdflayer/build.sh
+++ _pdflayer/build.sh
@@ -6,5 +6,12 @@ rm -rf layer
 docker build -t bweigel/ocr_layer . 
 CONTAINER=$(docker run -d bweigel/ocr_layer false)
 docker cp $CONTAINER:/opt/build-dist layer
+cd layer
+zip -r9 pdflayer.zip .
+aws s3 cp pdflayer.zip s3://com.shardul.s3formupload
+aws lambda publish-layer-version \
+    --layer-name ocrmypdf_layer \
+    --content S3Bucket=com.shardul.s3formupload,S3Key=pdflayer.zip 
 # docker rm $CONTAINER
+
 
