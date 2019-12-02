401781aa5a5927aa6ad5f2b25da5a83ef58fd314
secretshardul <shardul.aeer@somaiya.edu>
2019-12-02 17:23:39 +0530

    Chrono's python layer fixed
    GS 9.50 was  causing problem, downgraded to 9.27

--- _pdflayer/build.sh
+++ _pdflayer/build.sh
@@ -8,8 +8,8 @@ CONTAINER=$(docker run -d bweigel/ocr_layer false)
 docker cp $CONTAINER:/opt/build-dist layer
 cd layer
 zip -r9 pdflayer.zip .
-aws s3 cp pdflayer.zip s3://com.shardul.s3formupload
-aws lambda publish-layer-version \
+aws2 s3 cp pdflayer.zip s3://com.shardul.s3formupload
+aws2 lambda publish-layer-version \
     --layer-name ocrmypdf_layer \
     --content S3Bucket=com.shardul.s3formupload,S3Key=pdflayer.zip 
 # docker rm $CONTAINER
